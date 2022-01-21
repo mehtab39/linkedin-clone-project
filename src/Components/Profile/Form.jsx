6c7e2cf2782c1ef5ab81a54a8c75a82b00b50462
meera <shu@ahb.com>
2022-01-21 15:11:36 +0530

    connect data with firebase

--- src/Components/Profile/Form.jsx
+++ src/Components/Profile/Form.jsx
@@ -1,6 +1,43 @@
+import { useState } from "react";
 import styled from "styled-components";
-
+import { useAuth } from "../../contexts/AuthContext";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+const obj={
+    first_name: "",
+    last_name: "",
+    username: "",
+    address: "",
+    company: "",
+    experience: [],
+    education: [],
+    skills: [],
+    profile_summary: "",
+    job_title: "",
+    resume_path: "",
+    connections: [],
+    pending: [],
+    waiting: [],
+}
 export const Form=({handleClick})=>{
+    const {profile} = useAuth();
+    const dispatch = useDispatch()
+    const [form,setForm]=useState(obj)
+    const [data,setData]=useState([])
+    const handleChange=(e)=>
+    {
+        let {name,value}=e.target;
+        // if(name=="skills"||name=="education"||name=="experience")
+        // {
+        //     value=[...name,value]
+        // }
+        setForm({...form,[name]:value})
+    }
+  
+    const handleSubmit=(e)=>
+    {
+      e.preventDefault()
+    }
     return <FormData>
         <Head>
         <div>Enter your details</div>
@@ -8,29 +45,28 @@ export const Form=({handleClick})=>{
         <img src="/images/close.png" alt="" />
         </Btn>
         </Head>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
             <label htmlFor="">First name*</label>
-            <input type="text" />
+            <input name="first_name" onChange={handleChange} type="text" />
             <label htmlFor="">Last name*</label>
-            <input type="text" />
+            <input name="last_name" onChange={handleChange} type="text" />
             <label htmlFor="">Location</label>
-            <input type="text" />
+            <input name="address" onChange={handleChange} type="text" />
             <label htmlFor="">About</label>
-            <textarea></textarea>
-            <label htmlFor="">Education</label>            
-            <input type="text" />
+            <textarea name="profile_summary" onChange={handleChange}></textarea>
+             <label htmlFor="">Education</label>            
+            <input name="education" onChange={handleChange} type="text" />
             <label htmlFor="">Skills</label>            
-            <input type="text" />
-            <label htmlFor="">Company</label>            
-            <input type="text" />
+            <input name="skills" onChange={handleChange} type="text" />
+            {/* <label htmlFor="">Company</label>            
+            <input name="company" onChange={handleChange} type="text" />
             <label htmlFor="">Experience</label>            
-            <input type="text" />
-            <label htmlFor="">Profile Summary</label>            
-            <input type="text" />
+            <input name="experience" onChange={handleChange} type="text" />
             <label htmlFor="">Current Position</label>            
-            <input type="text" />
+            <input name="job_title" onChange={handleChange} type="text" />
             <label htmlFor="">Resume</label>            
-            <input type="text" />
+            <input name="resume_path" onChange={handleChange} type="text" />         */}
+            <input type="submit" />
         </form>
     </FormData>
 }
@@ -45,9 +81,10 @@ const Btn= styled.button`
 const FormData=styled.div`
     width:40%;
     margin: auto;
-    margin-top:4%;
+    margin-top:8%;
     display: flex;
-    overflow: absolute;
+    overflow: scroll;
+    height:400px;
     flex-direction: column;
     background-color: white;
     padding: 3%;
