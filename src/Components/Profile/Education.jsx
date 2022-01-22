df44432ae6576566fc3e70ec0e3c4c5dc7460c00
PUMMY SHARMA <pummy9799@gmail.com>
2022-01-22 23:20:32 +0530

    Profile

---
+++ src/Components/Profile/Education.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Education=()=>{
+    return <Abt>
+        <Div>
+            <Head>
+            <Title>Education</Title>      
+            </Head>
+            <Text>Add Education</Text>
+        </Div>
+    </Abt>
+}
+
+const Abt=styled.div`
+max-width: 100%;
+`;
+
+const Div=styled.div`
+    margin-top:30px;
+    border-radius: 5px;
+    display: flex;
+    background-color:white;
+    flex-direction: column;
+    width: 100%;
+    box-shadow: rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+    padding: 5%;
+    box-sizing: border-box;
+`;
+
+const Title = styled.p`
+    font-size: 20px;
+    font-weight: 600;
+`;
+const Img = styled.div`
+    width:26px;
+    margin-left:30px;
+    img{
+        width:100%;
+    }
+`;
+
+const Head=styled.div`
+    display:flex;
+    justify-content: space-between;
+`;
+
+const Text=styled.p`
+    font-size:13px;
+    color: rgba(0, 0, 0, 0.6);
+`;
