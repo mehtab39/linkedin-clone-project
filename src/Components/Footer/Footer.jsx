c4382f5e8c416fd8468935b68c90ab50d708e6ce
anandKmrSharma <87422058+anandKmrSharma@users.noreply.github.com>
2022-01-22 11:11:46 +0530

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./images/png0.png"
-import setting from "./images/png1.png"
-import question from "./images/png2.png"
+import question from "./images/png1.png"
+import setting from "./images/png2.png"
 import styled from "styled-components"
 function Footer() {
 	return (
@@ -32,7 +32,7 @@ function Footer() {
 					<div >Small Business</div>
 				</div>
 
-				<div style={{marginRight:"10px"}}>
+				<div style={{marginRight:"0px"}}>
 					<div><img style={{height:"30px"}} src={question} alt="" /></div>
 					<div style={{marginTop:"17px"}}><img style={{height:"30px"}}src={setting} alt="" /></div>
 				</div>
@@ -47,7 +47,7 @@ function Footer() {
 				</div>
 
 				<div>
-					<form>
+					<form >
 						<label style={{color:"#00000099", fontSize:"15px"}}>Select language</label><br />
 
 						<select style={{width:"250px", height:"30px", fontSize:"15px"}}>
@@ -78,12 +78,13 @@ function Footer() {
 const Box=styled.div `
 margin-top: 95%;
 margin-bottom: 10px;
+
 `
 ;
 const Image= styled.img`
 width:150px;
 height 40px;
-margin-left: 50px;
+margin-left: 60px;
 `
 ;
 
@@ -92,23 +93,29 @@ display:flex;
 background-color: #E0E0E0;
 margin-left: 50px;
 
-
 // @media (max-width: 700px) {
 //     flex-direction: column;
 //   }
+  
+
 
 & >div{
 	margin-right:60px;
+	padding:10px
+	// padding-left: 10px;
 	
 }
 
 & >div >div{
+	
 	
 	&:hover{
 		text-decoration: underline;
 		color:#536DFE;
 		}
 }
+
+
 `
 
 
